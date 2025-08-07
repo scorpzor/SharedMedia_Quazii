905553f806f73a257b660b6cb427bab4dae757a5
Quazii <contact@quazii.gg>
2025-08-07 23:13:50 +0800

    Update .toc file and add new sound file

---
+++ temp/SharedMedia_Quazii/Quazii.lua
@@ -0,0 +1,24 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+LSM:Register("font", "Quazii", [[Interface\Addons\SharedMedia_Quazii\font\Quazii.ttf]]) 
+
+-- -----
+--   SOUND
+-- -----
+
+-- -----
+--   TEXTURE --
+-- -----
+
+LSM:Register("statusbar", "Quazii", [[Interface\Addons\SharedMedia_Quazii\statusbar\Quazii.tga]]) 
